54ccc445fc2a4d5b4e5cebec2d8b71c8948a71fc
Azka Islam <azka.islam@rsa.com>
2020-08-23 15:01:46 -0400

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,11 @@
-console.log('Client Side JavaScript file is loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    console.log(location)
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
